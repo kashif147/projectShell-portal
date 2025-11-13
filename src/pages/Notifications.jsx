5ced3a2ac0394d3eb5209c2e6937a9099aa2d45b
fazal azim <fazalazim238@gmail.com>
2025-11-13 23:58:38 +0500

    feature/fix notification screen card

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -132,7 +132,7 @@ const Notifications = () => {
   const unreadCount = notifications.filter(n => !n.read).length;
 
   return (
-    <div className="min-h-screen bg-gray-50 py-6">
+    <div className="min-h-screen">
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         {/* Header */}
         <div className="mb-8">
