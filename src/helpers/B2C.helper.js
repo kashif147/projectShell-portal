fbc158b17a45fe4b0964fb1cd39be5a9e6cbbafd
Kashif <kashif147@gmail.com>
2025-11-23 18:22:32 +0000

    Update redirect URI in B2C helper to include '/auth' for Azure authentication

--- src/helpers/B2C.helper.js
+++ src/helpers/B2C.helper.js
@@ -15,7 +15,7 @@ export const microSoftUrlRedirect = async () => {
   authUrl.searchParams.append('nonce', 'defaultNonce');
   authUrl.searchParams.append(
     'redirect_uri',
-    'https://project-shell-portal.vercel.app',
+    'https://project-shell-portal.vercel.app/auth',
   );
   authUrl.searchParams.append('scope', 'openid offline_access');
   authUrl.searchParams.append('response_type', 'code');
