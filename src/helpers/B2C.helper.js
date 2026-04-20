7a158af8eb5e73abbc745134cc548d68d01418ef
fazal azim <fazalazim238@gmail.com>
2026-04-20 20:59:22 +0500

    feature/handle fix nagative payment

--- src/helpers/B2C.helper.js
+++ src/helpers/B2C.helper.js
@@ -18,3 +18,25 @@ export const microSoftUrlRedirect = async () => {
   authUrl.searchParams.append("code_challenge_method", "S256");
   window.location.href = authUrl.toString();
 };
+
+// export const microSoftUrlRedirect = async () => {
+//   const { code_verifier, code_challenge } = await generatePKCE();
+//   setVerifier(code_verifier);
+
+//   const authUrl = new URL(
+//     'https://projectshellab2c.b2clogin.com/projectshellAB2C.onmicrosoft.com/oauth2/v2.0/authorize'
+//   );
+
+//   authUrl.searchParams.append('p', 'B2C_1_projectshell_signin');
+//   authUrl.searchParams.append('client_id', 'b0a62557-3308-4efb-954a-fb4b6a787309');
+//   authUrl.searchParams.append('nonce', 'defaultNonce');
+//   authUrl.searchParams.append('redirect_uri', 'com.portal://com.portal/ios/callback');
+//   authUrl.searchParams.append('scope', 'openid offline_access');
+//   authUrl.searchParams.append('response_type', 'code');
+//   authUrl.searchParams.append('response_mode', 'query');
+//   authUrl.searchParams.append('prompt', 'login');
+//   authUrl.searchParams.append('code_challenge', code_challenge);
+//   authUrl.searchParams.append('code_challenge_method', 'S256');
+
+//   window.location.href = authUrl.toString();
+// };
